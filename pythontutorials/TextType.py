978421cc00f9891f50ccf69aa04f04df774de8ef
HR <hr080324@naver.com>
2024-03-09 23:27:19 +0900

    2024-03-09-11:27 Python tutorials

---
+++ pythontutorials/TextType.py
@@ -0,0 +1,11 @@
+print('hello world')
+print("hello world")
+print('''
+    Helllo
+    world
+    ''')
+
+print("'1' + '1'", '1' + '1')
+print ('Hello world' * 1000)
+print(len('Hello world' * 1000))
+print('Hello world'.replace('world', ' universe'))
